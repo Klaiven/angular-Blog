e431b36914328fffa4554e6ba9c24ef67b97bb17
klaiven <klaivencastro@gmail.com>
2024-02-24 01:44:29 -0300

    Wip: Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { AppModule } from './app.module';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppModule, MenuTitleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
